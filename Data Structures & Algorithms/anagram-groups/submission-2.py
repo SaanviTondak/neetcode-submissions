0509dfcd75780c3d6899e32c1b9b8c6ecbeda7b6
SaanviTondak <saanvitondak2004@gmail.com>
2026-09-21 00:51:04 +0800

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = defaultdict(list)
+
+        for string in strs:
+            freq = [0] * 26 #[0,0,0....,0]
+
+            for letter in string:
+                freq[ord(letter) - ord("a")] += 1 #ord func gives ascii value of letter
+            hashmap[tuple(freq)].append(string) #key in hashmap must be immutable hence tuple not list 
+        return list(hashmap.values())
+
+
+        #o(m * n)
+
+        
